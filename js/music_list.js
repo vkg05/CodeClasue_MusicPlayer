4f3806519e426f814baddb283803782284f3c62a
Vishal Gupta <vshal051198@gmail.com>
2023-08-16 17:41:49 +0530

    Music Player

---
+++ js/music_list.js
@@ -0,0 +1,54 @@
+let allMusic = [
+
+    {
+        name :"Tere Hawaale (From 'Laal Singh Chaddha')",
+        artist :"Pritam, Arijit Singh",
+        img :"music1",
+        src :"music1-1"
+    },
+    {
+        name :"Phir Na Aisi Raat (From 'Laal Singh Chaddha')",
+        artist :"Arijit Singh, Amitabh Bhattacharya",
+        img :"music2",
+        src :"music2-2"
+    },
+    {
+        name :"Phir Aur (From 'Zara Hatke Zara Bachke')",
+        artist :"Arijit Singh",
+        img :"music3",
+        src :"music3-3"
+    },
+    {
+        name :"Ram Siya Ram (From 'Ram Siya Ram')",
+        artist :"Sachet Tondan",
+        img :"music4",
+        src :"music4-4"
+    },
+    {
+        name :"Kesariya (From 'Brahmastra')",
+        artist :"Arijit Singh",
+        img :"music5",
+        src :"music5-5"
+    },
+    {
+        name :"Vande Bharatam (From 'T-Series')",
+        artist :"Vishal Mishra",
+        img :"music6",
+        src :"music6-6"
+    },
+    {
+        name :"Desh Mere (From 'Bhuj The Pride Of India')",
+        artist :" Arijit Singh, Arko",
+        img :"music7",
+        src :"music7-7"
+    },
+    
+    // To add more song, just copy the following code and paste inside the array
+    // like this paste it, and remember to give comma after ending of this bracket }
+    // {
+    //     name : "Here is the music name",
+    //     artist : "Here is the artist name",
+    //     img : "Here Image name remember img mush be in .jpg formate",
+    //     src: "Here Music name"
+    // },
+];
